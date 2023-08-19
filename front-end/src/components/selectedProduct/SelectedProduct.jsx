da1aab2e291f04b01a860cc4b70282e491a3a7a3
zeru0429 <mihiretutigistu@gmail.com>
2023-08-19 18:08:33 +0300

    checkout page prepared

---
+++ front-end/src/components/selectedProduct/SelectedProduct.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SelectedProduct = () => {
+  return (
+    <div>SelectedProduct</div>
+  )
+}
+
+export default SelectedProduct
--- front-end/src/pages/checkout/Checkout.jsx
+++ front-end/src/pages/checkout/Checkout.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './checkout.css'
+import { useStateValue } from '../../stateprovider';
+
+//components
+import Subtotal from "../../components/subtotal/Subtotal";
+import SelectedProduct from "../../components/selectedProduct/SelectedProduct";
+
+
+
+
+
 const Checkout = () => {
-  return (
-    <div>Checkout</div>
-  )
+    const [{ basket, user }, dispatch] = useStateValue();
+
+   return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+        />
+
+        <div>
+          <h3>
+            Hello,
+            {user?.email}
+          </h3>
+          <h2 className="checkout__title">Your shopping Basket</h2>
+
+          {basket.map((item, i) => (
+            <SelectedProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+              key={i}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
 }
 
 export default Checkout
