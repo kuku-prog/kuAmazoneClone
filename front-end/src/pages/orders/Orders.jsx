fa5820810feaa236069279aaff1434ad2e2053a8
zeru0429 <mihiretutigistu@gmail.com>
2023-08-19 18:47:54 +0300

    order component added

--- front-end/src/pages/orders/Orders.jsx
+++ front-end/src/pages/orders/Orders.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import './orders.css'
 
-const Orders = () => {
+import { useStateValue } from "../../stateprovider";
+import { db } from "../../firebase";
+import Order from "../../components/order/Order";
+
+
+
+function Orders() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const [orders, setOrders] = useState([]);
+  // console.log(orders);
+
+  useEffect(() => {
+    if (user) {
+      db.collection("users")
+        .doc(user?.uid)
+        .collection("orders")
+        .orderBy("created", "desc")
+        .onSnapshot((snapshot) =>
+          setOrders(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+          )
+        );
+      // console.log(snapshot);
+    } else {
+      setOrders([]);
+    }
+  }, [user]);
+
   return (
-    <div>Orders</div>
-  )
+    <div className="orders">
+      <h1>Your Orders</h1>
+      <div className="orders__order">
+        {orders?.map((order, i) => (
+          <Order order={order} key={i} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Orders
+export default Orders;
